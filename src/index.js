a8db37f753a5277ab516843dcad561f4c8e14e09
Mynor2397 <marold97@outlook.com>
2020-05-02 15:00:55 -0600

    Configuracion nodejs #124

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express =require('express');
+const morgan = require('morgan');
+const exphbs=require('express-handlebars'); 
+const path=require('path');
+
+
+//initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname,'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'list',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname: '.hbs'
+    
+}));
+app.set('view engine', '.hbs'); 
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false})); 
+app.use(express.json());
+
+//Routes
+app.use(require('./routes/covid'));
+
+//Starting the server
+app.listen(app.get('port'), ()=>{
+    console.log('Listen and serve on port: ', app.get('port'));
+});
